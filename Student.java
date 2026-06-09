f81e9bc8f5126e59693124c7a3d6412acefac3e7
zicoafgh <35202930+zicoafgh@users.noreply.github.com>
2026-06-09 07:13:47 +0000

    Methods

---
+++ Student.java
@@ -0,0 +1,31 @@
+
+class Student {
+
+    // Attributes
+    String name;
+    int age;
+    double gpa;
+
+    // Constructor
+    Student(String studentName, int studentAge, double studentGpa) {
+        name = studentName;
+        age = studentAge;
+        gpa = studentGpa;
+    }
+
+    // Methods
+    public void displayInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("GPA: " + gpa);
+    }
+
+    public void study() {
+        System.out.println(name + " is studying.");
+    }
+
+    public void takeExam() {
+        System.out.println(name + " is taking exam.");
+    }
+}
+    
