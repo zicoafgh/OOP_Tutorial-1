bebcedbab69b4e6bf742dcaf0e999e9454ee1286
zicoafgh <35202930+zicoafgh@users.noreply.github.com>
2026-08-05 07:05:31 +0100

    Delete week_11 directory

--- week_11/display.py
+++
@@ -1,9 +0,0 @@
-def print_result(name, student_id, status, reason):
-    # Display logic to show the access result
-    print("\n========== ACCESS RESULT ==========")
-    print(f"Student Name : {name}")
-    print(f"Student ID   : {student_id}")
-    print("-----------------------------------")
-    print(f"Status : {status}")
-    print(f"Reason : {reason}")
-    print("===================================")
